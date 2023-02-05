caa88858dc1027b59cf5680d84e4b99360939bc0
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-02-04 17:09:09 -0800

    Setup first page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import Head from "next/head";
 import "styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
-
+const MyApp: AppType = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>About Andres</title>
+      <meta
+        name="description"
+        content="Personal site about Anders Rodriguez Michel"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main className="mx-auto w-11/12 md:w-3/4 3xl:w-1/2">
+      <Component {...pageProps} />
+    </main>
+  </>
+);
 export default MyApp;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
+    },
+    extend: {
+      colors: {
+        Gray: "#484848",
+        DarkGray: "#181a1b",
+      },
+    },
   },
   plugins: [],
 };
