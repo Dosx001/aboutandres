1117809e5ff4981c4db97fb5e5fcc104a217a23d
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-02-09 10:45:38 -0800

    Updated homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ const MyApp: AppType = ({ Component, pageProps }) => (
       />
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <main className="box">
+    <main className="">
       <Component {...pageProps} />
     </main>
   </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,32 @@
 import MovingImg from "components/MovingImg";
-import Work from "components/Work";
 import { type NextPage } from "next";
 import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <>
-      <div className="color absolute left-0 h-full w-screen"></div>
-      <div className="absolute left-0 h-full w-screen bg-[url('https://cdn.pixabay.com/photo/2018/02/18/20/29/computer-3163437_960_720.png')]"></div>
-      <MovingImg delay={5} />
-      <MovingImg delay={6} />
-      <MovingImg delay={7} />
-      <MovingImg delay={8} />
-      <div className="box absolute z-10">
-        <h1 className="textshadow mx-auto text-center text-6xl font-bold text-white xl:text-8xl">
-          Hello! I&apos;m Andres Rodriguez Michel. I&apos;m a software engineer!
-        </h1>
-        <Image
-          className="mx-auto mt-4 rounded-full shadow-box"
-          loader={({ src, width }) => `${src}?w=${width}`}
-          src="https://avatars.githubusercontent.com/u/62494922"
-          alt="Picture of the author"
-          width={400}
-          height={400}
-        />
+      <div className="color overflow-hidden absolute h-full w-full">
+        <div className="left-0 h-full w-screen bg-[url('https://cdn.pixabay.com/photo/2018/02/18/20/29/computer-3163437_960_720.png')]">
+          <MovingImg delay={5} />
+          <MovingImg delay={6} />
+          <MovingImg delay={7} />
+          <MovingImg delay={8} />
+          <div className="box relative z-10">
+            <h1 className="textshadow mx-auto text-center text-6xl font-bold text-white xl:text-8xl">
+              Hello! I&apos;m Andres Rodriguez Michel. I&apos;m a software
+              engineer!
+            </h1>
+            <Image
+              className="mx-auto mt-4 rounded-full shadow-box"
+              loader={({ src, width }) => `${src}?w=${width}`}
+              src="https://avatars.githubusercontent.com/u/62494922"
+              alt="Picture of the author"
+              width={400}
+              height={400}
+            />
+          </div>
+        </div>
       </div>
-      <Work />
     </>
   );
 };
