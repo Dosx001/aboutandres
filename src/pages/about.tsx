2b9d4fa876d5d98925dd9264d07e5b52c0bf62e9
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-02-12 12:56:40 -0800

    Created glow animation

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,6 +3,7 @@ const About = () => {
     <svg
       viewBox="0 0 50 50"
       xmlns="http://www.w3.org/2000/svg"
+      className="glow fill-white"
     >
       <path d="M25 5L32 20L48 22L36 34L40 48L25 42L10 48L14 34L2 22L18 20" />
     </svg>
@@ -11,6 +12,7 @@ const About = () => {
     <svg
       viewBox="0 0 50 50"
       xmlns="http://www.w3.org/2000/svg"
+      className="glow fill-white"
     >
       <path d="M5 25L20 20L25 5L30 20L45 25L30 30L25 45L20 30" />
     </svg>
@@ -19,6 +21,7 @@ const About = () => {
     <svg
       viewBox="0 0 35 35"
       xmlns="http://www.w3.org/2000/svg"
+      className="glow fill-white"
     >
       <path d="M20 2A1 1 0 0 0 20 32A18 18 0 0 1 20 2z" />
     </svg>
@@ -27,12 +30,14 @@ const About = () => {
     <svg
       viewBox="0 0 10 10"
       xmlns="http://www.w3.org/2000/svg"
+      className="glow fill-white"
     >
       <circle cx="5" cy="5" r="4" />
     </svg>
   );
   return (
     <>
+      <div className="absolute h-screen w-screen bg-black" />
       <div className="box relative z-10">
         <div className="flex">
           {cresent}
