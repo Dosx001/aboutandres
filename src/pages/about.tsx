fe7b6bea5d3ed54ae8c5bdcdbadd7ad01c4f9262
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-02-12 20:00:40 -0800

    Created rainbow color glow animations

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,37 +1,21 @@
 const About = () => {
   const star = (
-    <svg
-      viewBox="0 0 50 50"
-      xmlns="http://www.w3.org/2000/svg"
-      className="glow fill-white"
-    >
+    <svg viewBox="0 0 50 50" className="glow-orange fill-[orange]">
       <path d="M25 5L32 20L48 22L36 34L40 48L25 42L10 48L14 34L2 22L18 20" />
     </svg>
   );
   const star4 = (
-    <svg
-      viewBox="0 0 50 50"
-      xmlns="http://www.w3.org/2000/svg"
-      className="glow fill-white"
-    >
+    <svg viewBox="0 0 50 50" className="glow-yellow fill-[yellow]">
       <path d="M5 25L20 20L25 5L30 20L45 25L30 30L25 45L20 30" />
     </svg>
   );
   const cresent = (
-    <svg
-      viewBox="0 0 35 35"
-      xmlns="http://www.w3.org/2000/svg"
-      className="glow fill-white"
-    >
+    <svg viewBox="0 0 35 35" className="glow-red fill-[red]">
       <path d="M20 2A1 1 0 0 0 20 32A18 18 0 0 1 20 2z" />
     </svg>
   );
   const moon = (
-    <svg
-      viewBox="0 0 10 10"
-      xmlns="http://www.w3.org/2000/svg"
-      className="glow fill-white"
-    >
+    <svg viewBox="0 0 10 10" className="glow-green fill-[green]">
       <circle cx="5" cy="5" r="4" />
     </svg>
   );
