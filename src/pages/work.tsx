fa41d07d3d683a7c499a326a3e9e17dda30a55c2
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-02-18 15:58:13 -0800

    Added framer motion

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 const Work = () => {
@@ -33,16 +34,32 @@ const Work = () => {
         <div className="inline-block h-full w-screen bg-[url('http://www.script-tutorials.com/demos/360/images/stars.png')]" />
         <div className="inline-block h-full w-screen bg-[url('http://www.script-tutorials.com/demos/360/images/stars.png')]" />
       </div>
-      <h1 className="pt-4 text-center text-4xl">Work Experience</h1>
+      <motion.h1
+        className="pt-4 text-center text-4xl"
+        initial={{ y: -100 }}
+        animate={{ y: 0 }}
+      >
+        Work Experience
+      </motion.h1>
       <div className="relative mt-1 flex h-[80vh] justify-center overflow-y-auto">
         <table>
-          {jobs.map((job) => (
+          {jobs.map((job, i) => (
             <tr key={job.company}>
-              <td className="whitespace-nowrap px-4 text-center">
+              <motion.td
+                className="whitespace-nowrap px-4 text-center"
+                initial={{ x: -200, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ delay: i * 0.2 }}
+              >
                 <div className="border-b-2">{job.end}</div>
                 <div>{job.start}</div>
-              </td>
-              <td className="w-32 border-l-2">
+              </motion.td>
+              <motion.td
+                className="w-32 border-l-2"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 9 }}
+                transition={{ delay: i * 0.2 }}
+              >
                 <Image
                   loader={({ src, width }) => `${src}?w=${width}`}
                   src={job.logo}
@@ -51,12 +68,16 @@ const Work = () => {
                   height={100}
                   className="my-2 ml-2 border opacity-90"
                 />
-              </td>
-              <td>
+              </motion.td>
+              <motion.td
+                initial={{ x: 200, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ delay: i * 0.2 }}
+              >
                 <h2 className="text-2xl text-white">{job.company}</h2>
                 <div className="text-blue-300">{job.positon}</div>
                 <div className="text-gray-400">{job.location}</div>
-              </td>
+              </motion.td>
             </tr>
           ))}
         </table>
