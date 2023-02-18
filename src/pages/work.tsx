196ed63ea11563a61fac743a7a3284ce5e473522
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-02-17 18:19:04 -0800

    Remade work page

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,49 +1,30 @@
-import ImgSlider from "components/ImgSlider";
 import Image from "next/image";
-import { useState } from "react";
 
 const Work = () => {
-  const [x, setX] = useState(0);
-  const info = [
+  const jobs = [
     {
-      imgs: [
-        "https://www.baesystems.com/en-media/webImage/20210706095016/1434667302689.jpg",
-      ],
-      txt: [
-        "Integrate and test Linux software on military equipment",
-        "Developed shell scripts to automate daily tasks such turning off lab equipment",
-        "Documented and track issues using Jira and communicated with team on resolution",
-        "Utilize MySQL to keep track test results for regression tests and engineering checks",
-        "Debug software using SQL by checking health events with MariaDB",
-      ],
+      company: "BAE Systems",
+      positon: "Software Engineer",
+      location: "San Jose",
+      start: "Jan 2022",
+      end: "Present",
+      logo: "https://www.rbsscaffolding.co.uk/wp-content/uploads/2017/08/BAE-Systems-Logo-600px-280x280.png",
     },
     {
-      imgs: [
-        "https://i.imgur.com/KBrneBB.jpg",
-        "https://i.imgur.com/5y3csg1.jpg",
-        "https://i.imgur.com/WzDW6Is.jpg",
-        "https://i.imgur.com/qSKetQi.jpg",
-        "https://i.imgur.com/sHkuFkK.jpg",
-        "https://i.imgur.com/IqzAdj5.jpg",
-        "https://i.imgur.com/PIHJCzV.jpg",
-        "https://i.imgur.com/KAAYo8i.jpg",
-        "https://i.imgur.com/9HBXg29.jpg",
-      ],
-      txt: [
-        "Analyzed hundreds of simulations using Python, Matplotlib, and NumPy",
-        "Assisted creating an array of radio antennas to detect high energy cosmic neutrinos",
-        "Independently worked on simulating, prototyping, and constructing antennas",
-        "Communicated with a diverse multidisciplinary team from three universities",
-      ],
+      company: "BEACON",
+      positon: "Laboratory Assistant",
+      location: "San Luis Obispo",
+      start: "Jan 2017",
+      end: "Aug 2020",
+      logo: "https://bpb-us-e1.wpmucdn.com/sites.psu.edu/dist/7/120451/files/2021/07/BEACON-logo-v3-transparent.png",
     },
     {
-      imgs: [
-        "https://ssgse.com/ssg/wp-content/uploads/SSG-CalPoly-CampusMktUU-1-1024x617.jpg",
-      ],
-      txt: [
-        "Worked in fast-paced dynamic environment by role switching in critical moments",
-        "Tracked and manage numerous tasks such cash register, stocks, food stations, etc",
-      ],
+      company: "Cal Poly Campus Dining",
+      positon: "Student Assistant",
+      location: "San Luis Obispo",
+      start: "Sept 2016",
+      end: "Mar 2017",
+      logo: "https://res.cloudinary.com/doa6grfya/image/upload/v1519824006/lclnzs6ibymktn0lvn9f.jpg",
     },
   ];
   return (
@@ -55,39 +36,31 @@ const Work = () => {
       <div className="box relative z-10">
         <h1 className="pt-4 text-center text-4xl">Work Experience</h1>
         <div className="mx-auto mt-1 h-[80vh] w-11/12 rounded bg-gray-600 p-1">
-          <div className="flex justify-evenly">
-            <button
-              className="w-full rounded-l border border-gray-500 bg-black p-1"
-              style={{ textDecoration: x === 0 ? "underline cyan" : "" }}
-              onClick={() => setX(0)}
-            >
-              Software Engineer | BAE Systems
-            </button>
-            <button
-              className="w-full border border-gray-500 bg-black p-1"
-              style={{ textDecoration: x === 1 ? "underline cyan" : "" }}
-              onClick={() => setX(1)}
-            >
-              Lab Assistant | BEACON
-            </button>
-            <button
-              className="w-full rounded-r border border-gray-500 bg-black p-1"
-              style={{ textDecoration: x === 2 ? "underline cyan" : "" }}
-              onClick={() => setX(2)}
-            >
-              Student Assistant | Cal Poly Dining
-            </button>
-          </div>
-          <div className="block w-full pt-1 md:flex">
-            <ImgSlider imgs={info[x]!.imgs} />
-            <div className="float-left">
-              <ul>
-                {info[x]!.txt.map((t, i) => (
-                  <li key={i}>{t}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
+          <table className="">
+            {jobs.map((job) => (
+              <tr key={job.company}>
+                <td className="whitespace-nowrap px-4 text-center">
+                  <div className="border-b-2 border-[navy]">{job.start}</div>
+                  <div>{job.end}</div>
+                </td>
+                <td className="w-32 border-l-2 border-[navy]">
+                  <Image
+                    loader={({ src, width }) => `${src}?w=${width}`}
+                    src={job.logo}
+                    alt=""
+                    width={100}
+                    height={100}
+                    className="my-2 ml-2 border border-[navy]"
+                  />
+                </td>
+                <td className="">
+                  <h2 className="text-2xl text-white">{job.company}</h2>
+                  <div className="text-[navy]">{job.positon}</div>
+                  <div>{job.location}</div>
+                </td>
+              </tr>
+            ))}
+          </table>
         </div>
       </div>
     </div>
