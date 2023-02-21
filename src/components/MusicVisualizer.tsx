768777d84f8a3c998aed24192e063767ee55f0fa
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-02-20 18:15:23 -0800

    Reworked MusicVisualizer

--- src/components/MusicVisualizer.tsx
+++ src/components/MusicVisualizer.tsx
@@ -5,6 +5,10 @@ const MusicVisualizer = () => {
   const audioRef = useRef<HTMLAudioElement>(null);
   const [audioContext, setAudioContext] = useState(new AudioContext());
   const [track, setTrack] = useState(0);
+  const [playing, setPlaying] = useState(false);
+  const [muted, setMuted] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
   const tracks = [
     "Count What You Have Now",
     "Bay City",
@@ -17,7 +21,7 @@ const MusicVisualizer = () => {
     const canvas = canvasRef.current!;
     const audio = audioRef.current!;
     const ctx = canvas.getContext("2d")!;
-    canvas.width = window.innerWidth;
+    canvas.width = canvas.parentElement!.clientWidth;
     canvas.height = window.innerHeight;
     const analyserNode = audioContext.createAnalyser();
     analyserNode.fftSize = 256;
@@ -36,7 +40,12 @@ const MusicVisualizer = () => {
     };
     audio.src = `music/${tracks[track]!}.mp3`;
     audio.load();
-    audio.play().catch((err) => console.debug(err));
+    audio
+      .play()
+      .then(() => setPlaying(true))
+      .catch((err) => console.debug(err));
+    setDuration(audio.duration);
+    audio.ontimeupdate = () => setCurrentTime(audio.currentTime);
     renderFrame();
     return () => {
       audioContext.close().catch((err) => console.debug(err));
@@ -44,47 +53,103 @@ const MusicVisualizer = () => {
     };
   }, [track]);
   const next = () => setTrack(track === tracks.length - 1 ? 0 : track + 1);
+  const prev = () => setTrack(track === 0 ? tracks.length - 1 : track - 1);
+  const play = () => {
+    playing
+      ? audioRef.current!.pause()
+      : audioRef.current!.play().catch((err) => console.debug(err));
+    setPlaying(!playing);
+  };
+  const mute = () => {
+    audioRef.current!.muted = !muted;
+    setMuted(!muted);
+  };
+  const formatTime = (seconds: number) => {
+    const mins = Math.floor(seconds / 60);
+    const secs = Math.floor(seconds % 60);
+    return `${mins < 10 ? `${mins}` : mins}:${secs < 10 ? `0${secs}` : secs}`;
+  };
   return (
-    <div className="fixed bottom-10">
+    <div className="fixed bottom-10 w-11/12 whitespace-nowrap md:w-3/4 3xl:w-1/2">
+      <audio
+        ref={audioRef}
+        onEnded={next}
+        onLoadedMetadata={() => setDuration(audioRef.current!.duration)}
+        onTimeUpdate={() => setCurrentTime(audioRef.current!.currentTime)}
+      />
       <canvas ref={canvasRef} />
-      <div className="flex w-fit whitespace-nowrap rounded-b shadow shadow-black">
-        <div className="bg-[#2e2e2e] pt-2 pl-3 md:pr-0">
-          Track: {track + 1}/{tracks.length}
+      <div className="rounded-b bg-[#2e2e2e] py-1 px-2 shadow shadow-black">
+        <div>
+          Track {track + 1}/{tracks.length} : {tracks[track]!}
         </div>
-        <button
-          className="bg-[#2e2e2e] pl-3 hover:cursor-default"
-          onClick={() => setTrack(track === 0 ? tracks.length - 1 : track - 1)}
-        >
-          <svg
-            width="15"
-            height="15"
-            viewBox="0 0 12 12"
-            className="fill-white hover:fill-[#00b6f0]"
-          >
-            <path d="M2 1v10h-2v-10h1zm9 0l-8 5 8 5-10z" />
-          </svg>
-        </button>
-        <button
-          className="bg-[#2e2e2e] pl-3 hover:cursor-default"
-          onClick={next}
-        >
-          <svg
-            width="15"
-            height="15"
-            viewBox="0 0 12 12"
-            className="rotate-180 fill-white hover:fill-[#00b6f0]"
-          >
-            <path d="M2 1v10h-2v-10h1zm9 0l-8 5 8 5-10z" />
-          </svg>
-        </button>
-        <audio
-          controls
-          ref={audioRef}
-          className="w-[60vw] md:w-[30vw] lg:w-[35vw] xl:w-[40vw]"
-          onEnded={next}
-        />
-        <div className="hidden w-52 rounded-br bg-[#2e2e2e] pt-2 md:block">
-          {tracks[track]!}
+        <div className="flex">
+          <button className="" onClick={prev}>
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 12 12"
+              className="fill-white hover:fill-[#00b6f0]"
+            >
+              <path d="M2 1v10h-2v-10h1zm9 0l-8 5 8 5-10z" />
+            </svg>
+          </button>
+          <button onClick={play}>
+            <svg
+              width="25"
+              height="25"
+              viewBox="0 0 20 20"
+              className="fill-white hover:fill-[#00b6f0]"
+            >
+              {playing ? (
+                <path d="M5 4H7V16H5ZM13 4H15V16H13Z" />
+              ) : (
+                <path d="M6 4L16 10L6 16Z" />
+              )}
+            </svg>
+          </button>
+          <button className="" onClick={next}>
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 12 12"
+              className="rotate-180 fill-white hover:fill-[#00b6f0]"
+            >
+              <path d="M2 1v10h-2v-10h1zm9 0l-8 5 8 5-10z" />
+            </svg>
+          </button>
+          <button onClick={mute}>
+            <svg
+              width="25"
+              height="25"
+              viewBox="0 0 75 75"
+              className="mx-2 fill-white stroke-white stroke-[4] hover:fill-[#00b6f0] hover:stroke-[#00b6f0]"
+            >
+              <path d="M39.389 13.769L22.235 28.606L6 28.606L6 47.699L21.989 47.699L39.389 62.75L39.389 13.769z" />
+              {muted ? (
+                <path className="fill-none" d="m49 26 20 24m0-24-20 24" />
+              ) : (
+                <path
+                  className="fill-none"
+                  d="M48 27.6a19.5 19.5 0 0 1 0 21.4M55.1 20.5a30 30 0 0 1 0 35.6M61.6 14a38.8 38.8 0 0 1 0 48.6"
+                />
+              )}
+            </svg>
+          </button>
+          <input
+            className="w-full cursor-pointer"
+            type="range"
+            min={0}
+            max={duration}
+            value={currentTime}
+            step={0.1}
+            onChange={(ev) => {
+              setCurrentTime(parseFloat(ev.target.value));
+              audioRef.current!.currentTime = parseFloat(ev.target.value);
+            }}
+          />
+          <div className="ml-2">
+            {formatTime(currentTime)} / {formatTime(duration)}
+          </div>
         </div>
       </div>
     </div>
