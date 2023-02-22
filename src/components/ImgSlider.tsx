fc88fd97134d06617466a950e2bf4a35d8f65885
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-02-21 17:21:50 -0800

    Added animations to ImgSlider

--- src/components/ImgSlider.tsx
+++ src/components/ImgSlider.tsx
@@ -1,11 +1,18 @@
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const ImgSlider = ({ imgs }: { imgs: string[] }) => {
   const [index, setIndex] = useState(0);
-  const nextImg = () => setIndex(index === imgs.length - 1 ? 0 : index + 1);
-  const prevImg = () => setIndex(index === 0 ? imgs.length - 1 : index - 1);
-  useEffect(() => setIndex(0), [imgs]);
+  const [x, setX] = useState(0);
+  const nextImg = () => {
+    setIndex(index === imgs.length - 1 ? 0 : index + 1);
+    setX(-1000);
+  };
+  const prevImg = () => {
+    setIndex(index === 0 ? imgs.length - 1 : index - 1);
+    setX(1000);
+  };
   return (
     <>
       <div className="block">
@@ -33,20 +40,31 @@ const ImgSlider = ({ imgs }: { imgs: string[] }) => {
           </div>
         </div>
         <div>
-          {imgs.map(
-            (img, i) =>
-              index === i && (
-                <Image
-                  key={i}
-                  loader={({ src, width }) => `${src}?w=${width}`}
-                  src={img}
-                  alt=""
-                  width={400}
-                  height={400}
-                  className="mx-auto w-full"
-                />
-              )
-          )}
+          <AnimatePresence>
+            {imgs.map(
+              (img, i) =>
+                index === i && (
+                  <motion.div
+                    initial={{ opacity: 0, x: x }}
+                    animate={{ opacity: 1, x: 0, transition: { delay: 0.3 } }}
+                    exit={{
+                      scale: 0,
+                      transition: { delay: 0 },
+                    }}
+                    key={i}
+                  >
+                    <Image
+                      loader={({ src, width }) => `${src}?w=${width}`}
+                      src={img}
+                      alt=""
+                      width={500}
+                      height={500}
+                      className="mx-auto w-full"
+                    />
+                  </motion.div>
+                )
+            )}
+          </AnimatePresence>
         </div>
       </div>
     </>
