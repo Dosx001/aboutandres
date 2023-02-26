3b8d1c7177a950fe9070da1fdaf735f478535920
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-02-26 12:23:38 -0800

    Finished adding content to Cube

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -21,31 +21,39 @@ const Cube = () => {
   };
   return (
     <div className="mt-10">
+      <Image
+        className="fixed h-1 w-1 opacity-0"
+        loader={({ src, width }) => `${src}?w=${width}`}
+        src="/interests/eureka.gif"
+        alt="Eureka Seven gif"
+        width={400}
+        height={400}
+      />
       <div
         className="preserve-3d mx-auto h-[26rem] w-[26rem]"
         style={{ transform: `rotate3d(${rotation()})` }}
       >
         <div
-          className="face bg-red-600"
+          className="face bg-red-700"
           style={{
             transform: "translateZ(13rem)",
           }}
         >
           <Image
-            className="p-1"
+            className="mx-auto p-1"
             loader={({ src, width }) => `${src}?w=${width}`}
             src="https://camo.githubusercontent.com/c11f5ba77a87e59ae7995d2ac4a4edcff674e0ff63fb3ab28142b9397e204ca7/68747470733a2f2f6769746875622d726561646d652d73746174732e76657263656c2e6170702f6170692f3f757365726e616d653d446f737830303126636f756e745f707269766174653d7472756526696e636c7564655f616c6c5f636f6d6d6974733d747275652673686f775f69636f6e733d74727565267469746c655f636f6c6f723d6666662669636f6e5f636f6c6f723d30306537666626746578745f636f6c6f723d3966396639662662675f636f6c6f723d313531353135"
             alt="Top Languages"
             width={400}
-            height={400}
+            height={200}
           />
           <Image
-            className="max-h-52 p-1"
+            className="mx-auto max-h-52 p-1"
             loader={({ src, width }) => `${src}?w=${width}`}
             src="https://camo.githubusercontent.com/774520facfb92575c862b7a2b4b23264e13b0e41067f58fd38bad38415fde88f/68747470733a2f2f6769746875622d726561646d652d73746174732e76657263656c2e6170702f6170692f746f702d6c616e67732f3f757365726e616d653d446f7378303031266c616e67735f636f756e743d3130266c61796f75743d636f6d70616374267469746c655f636f6c6f723d66666626746578745f636f6c6f723d3030653766662662675f636f6c6f723d313531353135"
             alt="Top Languages"
             width={400}
-            height={400}
+            height={200}
           />
         </div>
         <div
@@ -63,12 +71,12 @@ const Cube = () => {
             height={400}
           />
           <Image
-            className="p-1"
+            className="mx-auto p-1"
             loader={({ src, width }) => `${src}?w=${width}`}
             src="/interests/desktop.png"
             alt="Top Languages"
-            width={800}
-            height={800}
+            width={400}
+            height={400}
           />
         </div>
         <div
@@ -77,7 +85,14 @@ const Cube = () => {
             transform: "rotateY(90deg) translateZ(13rem)",
           }}
         >
-          Gaming
+          <Image
+            className="p-1"
+            loader={({ src, width }) => `${src}?w=${width}`}
+            src="/interests/games.png"
+            alt="Top Languages"
+            width={400}
+            height={400}
+          />
         </div>
         <div
           className="face bg-green-700"
@@ -85,7 +100,14 @@ const Cube = () => {
             transform: "rotateY(-90deg) translateZ(13rem)",
           }}
         >
-          Anime
+          <Image
+            className="mx-auto mt-10 p-1"
+            loader={({ src, width }) => `${src}?w=${width}`}
+            src="/interests/eureka.gif"
+            alt="Eureka Seven gif"
+            width={400}
+            height={400}
+          />
         </div>
         <div
           className="face bg-blue-700"
@@ -116,7 +138,14 @@ const Cube = () => {
             transform: "rotateX(-90deg) translateZ(13rem)",
           }}
         >
-          FOSS
+          <Image
+            className="p-1"
+            loader={({ src, width }) => `${src}?w=${width}`}
+            src="/interests/foss.png"
+            alt="Top Languages"
+            width={800}
+            height={800}
+          />
         </div>
       </div>
       <div className="cube">
