413a0792f4afe6f64a5892cc03ff8fd606261273
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-03-03 18:37:51 -0800

    Clean up Cube

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -42,15 +42,15 @@ const Cube = () => {
           <Image
             className="mx-auto p-1"
             loader={({ src, width }) => `${src}?w=${width}`}
-            src="https://camo.githubusercontent.com/c11f5ba77a87e59ae7995d2ac4a4edcff674e0ff63fb3ab28142b9397e204ca7/68747470733a2f2f6769746875622d726561646d652d73746174732e76657263656c2e6170702f6170692f3f757365726e616d653d446f737830303126636f756e745f707269766174653d7472756526696e636c7564655f616c6c5f636f6d6d6974733d747275652673686f775f69636f6e733d74727565267469746c655f636f6c6f723d6666662669636f6e5f636f6c6f723d30306537666626746578745f636f6c6f723d3966396639662662675f636f6c6f723d313531353135"
-            alt="Top Languages"
+            src="https://camo.githubusercontent.com/7448e71083ff40bfc766420d5350ca6f8c29e627269a445af0811e3453c6eefb/68747470733a2f2f6769746875622d726561646d652d73746174732d646f73783030312e76657263656c2e6170702f6170692f3f757365726e616d653d446f737830303126636f756e745f707269766174653d7472756526696e636c7564655f616c6c5f636f6d6d6974733d747275652673686f775f69636f6e733d74727565267469746c655f636f6c6f723d6666662669636f6e5f636f6c6f723d30306537666626746578745f636f6c6f723d3966396639662662675f636f6c6f723d313531353135"
+            alt="GitHub stats"
             width={400}
             height={200}
           />
           <Image
             className="mx-auto max-h-52 p-1"
             loader={({ src, width }) => `${src}?w=${width}`}
-            src="https://camo.githubusercontent.com/774520facfb92575c862b7a2b4b23264e13b0e41067f58fd38bad38415fde88f/68747470733a2f2f6769746875622d726561646d652d73746174732e76657263656c2e6170702f6170692f746f702d6c616e67732f3f757365726e616d653d446f7378303031266c616e67735f636f756e743d3130266c61796f75743d636f6d70616374267469746c655f636f6c6f723d66666626746578745f636f6c6f723d3030653766662662675f636f6c6f723d313531353135"
+            src="https://camo.githubusercontent.com/225b77cd02b1c2ff3f25ad079e657a9e23bfc5faf17d67f146c4d905d1044f58/68747470733a2f2f6769746875622d726561646d652d73746174732d646f73783030312e76657263656c2e6170702f6170692f746f702d6c616e67732f3f757365726e616d653d446f7378303031266c616e67735f636f756e743d3130266c61796f75743d636f6d70616374267469746c655f636f6c6f723d66666626746578745f636f6c6f723d3030653766662662675f636f6c6f723d313531353135"
             alt="Top Languages"
             width={400}
             height={200}
@@ -66,7 +66,7 @@ const Cube = () => {
             className="p-1"
             loader={({ src, width }) => `${src}?w=${width}`}
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Archlinux-logo-standard-version.png/2560px-Archlinux-logo-standard-version.png"
-            alt="Top Languages"
+            alt="Arch Linux logo"
             width={400}
             height={400}
           />
@@ -74,7 +74,7 @@ const Cube = () => {
             className="mx-auto p-1"
             loader={({ src, width }) => `${src}?w=${width}`}
             src="/interests/desktop.png"
-            alt="Top Languages"
+            alt="My desktop environment"
             width={400}
             height={400}
           />
@@ -89,7 +89,7 @@ const Cube = () => {
             className="p-1"
             loader={({ src, width }) => `${src}?w=${width}`}
             src="/interests/games.png"
-            alt="Top Languages"
+            alt="Collage of my favorite games"
             width={400}
             height={400}
           />
@@ -127,7 +127,7 @@ const Cube = () => {
             className="p-1"
             loader={({ src, width }) => `${src}?w=${width}`}
             src="/interests/nvim.png"
-            alt="Top Languages"
+            alt="My NeoVim setup"
             width={800}
             height={800}
           />
@@ -142,49 +142,23 @@ const Cube = () => {
             className="p-1"
             loader={({ src, width }) => `${src}?w=${width}`}
             src="/interests/foss.png"
-            alt="Top Languages"
+            alt="Collage of FOSS software"
             width={800}
             height={800}
           />
         </div>
       </div>
-      <div className="cube">
-        <button
-          onClick={() => setFace(0)}
-          style={{ background: face === 0 ? "pink" : "" }}
-        >
-          Coding
-        </button>
-        <button
-          onClick={() => setFace(1)}
-          style={{ background: face === 1 ? "pink" : "" }}
-        >
-          Linux
-        </button>
-        <button
-          onClick={() => setFace(2)}
-          style={{ background: face === 2 ? "pink" : "" }}
-        >
-          Vim
-        </button>
-        <button
-          onClick={() => setFace(3)}
-          style={{ background: face === 3 ? "pink" : "" }}
-        >
-          FOSS
-        </button>
-        <button
-          onClick={() => setFace(4)}
-          style={{ background: face === 4 ? "pink" : "" }}
-        >
-          Anime
-        </button>
-        <button
-          onClick={() => setFace(5)}
-          style={{ background: face === 5 ? "pink" : "" }}
-        >
-          Gaming
-        </button>
+      <div className="mx-auto grid grid-cols-3 md:w-1/2">
+        {["Coding", "Linux", "Vim", "FOSS", "Anime", "Gaming"].map((str, i) => (
+          <button
+            key={i}
+            className="m-1 rounded bg-[#2e2e2e] p-1 shadow shadow-black hover:bg-LightBlue"
+            onClick={() => setFace(i)}
+            style={{ background: face === i ? "pink" : "" }}
+          >
+            {str}
+          </button>
+        ))}
       </div>
     </div>
   );
