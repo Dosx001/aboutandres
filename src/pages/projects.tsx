caf5701da38643aa4cea799d9d6596c10c95b853
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-07-18 06:52:38 -0700

    refactor(page): switch page animations

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -17,33 +17,36 @@ const Projects = () => {
   ];
   return (
     <>
-      <div className="absolute h-1/2 w-full bg-black"></div>
-      <div className="absolute mt-[50vh] h-1/2 w-full bg-gray-500"></div>
-      <div className="ripple absolute z-10 mt-[50vh] w-full"></div>
-      <div className="box relative z-20">
-        <motion.h1
-          className="pt-4 text-center text-4xl"
-          initial={{ y: -100 }}
-          animate={{ y: 0 }}
-        >
-          Personal Projects
-        </motion.h1>
-        <div className="flex justify-between overflow-x-auto whitespace-nowrap">
-          {projs.map((proj, i) => (
-            <motion.button
-              className="mx-1 my-2 rounded bg-gray-500 py-1 px-2 hover:animate-pulse"
-              key={proj}
-              onClick={() => setName(proj)}
-              initial={{ x: 200, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{ delay: i * 0.1 }}
-              style={{ background: proj === name ? "#4bc05a" : "" }}
-            >
-              {proj}
-            </motion.button>
-          ))}
+      <div className="h-screen w-screen bg-gradient-to-t from-[navy] to-black">
+        <div className="star absolute h-full w-[200vw]">
+          <div className="inline-block h-full w-screen bg-[url('/work/stars.png')]" />
+          <div className="inline-block h-full w-screen bg-[url('/work/stars.png')]" />
         </div>
-        <Project name={name} />
+        <div className="box relative z-20">
+          <motion.h1
+            className="pt-4 text-center text-4xl"
+            initial={{ y: -100 }}
+            animate={{ y: 0 }}
+          >
+            Personal Projects
+          </motion.h1>
+          <div className="flex justify-between overflow-x-auto whitespace-nowrap">
+            {projs.map((proj, i) => (
+              <motion.button
+                className="mx-1 my-2 rounded bg-gray-500 py-1 px-2 hover:animate-pulse"
+                key={proj}
+                onClick={() => setName(proj)}
+                initial={{ x: 200, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ delay: i * 0.1 }}
+                style={{ background: proj === name ? "navy" : "" }}
+              >
+                {proj}
+              </motion.button>
+            ))}
+          </div>
+          <Project name={name} />
+        </div>
       </div>
     </>
   );
--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -37,58 +37,59 @@ const Work = () => {
     },
   ];
   return (
-    <div className="h-screen w-screen bg-gradient-to-t from-[navy] to-black">
-      <div className="star absolute h-full w-[200vw]">
-        <div className="inline-block h-full w-screen bg-[url('/work/stars.png')]" />
-        <div className="inline-block h-full w-screen bg-[url('/work/stars.png')]" />
-      </div>
-      <motion.h1
-        className="pt-4 text-center text-4xl"
-        initial={{ y: -100 }}
-        animate={{ y: 0 }}
-      >
-        Work Experience
-      </motion.h1>
-      <div className="relative mt-1 flex h-[80vh] justify-center overflow-y-auto">
-        <table>
-          {jobs.map((job, i) => (
-            <tr key={job.company}>
-              <motion.td
-                className="whitespace-nowrap px-4 text-center"
-                initial={{ x: -200, opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-                transition={{ delay: i * 0.2 }}
-              >
-                <div className="border-b-2">{job.end}</div>
-                <div>{job.start}</div>
-              </motion.td>
-              <motion.td
-                className="w-32 border-l-2"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 9 }}
-                transition={{ delay: i * 0.2 }}
-              >
-                <Image
-                  loader={({ src, width }) => `${src}?w=${width}`}
-                  src={`work/${job.logo}.png`}
-                  alt=""
-                  width={100}
-                  height={100}
-                  className="my-2 ml-2 border opacity-90"
-                />
-              </motion.td>
-              <motion.td
-                initial={{ x: 200, opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-                transition={{ delay: i * 0.2 }}
-              >
-                <h2 className="text-2xl">{job.company}</h2>
-                <div className="text-blue-300">{job.positon}</div>
-                <div className="text-gray-400">{job.location}</div>
-              </motion.td>
-            </tr>
-          ))}
-        </table>
+    <div>
+      <div className="absolute h-1/2 w-full bg-black"></div>
+      <div className="absolute mt-[50vh] h-1/2 w-full bg-gray-500"></div>
+      <div className="ripple absolute z-10 mt-[50vh] w-full"></div>
+      <div className="box relative z-20">
+        <motion.h1
+          className="pt-4 text-center text-4xl"
+          initial={{ y: -100 }}
+          animate={{ y: 0 }}
+        >
+          Work Experience
+        </motion.h1>
+        <div className="relative mt-1 flex h-[80vh] justify-center overflow-y-auto">
+          <table>
+            {jobs.map((job, i) => (
+              <tr key={job.company}>
+                <motion.td
+                  className="whitespace-nowrap px-4 text-center"
+                  initial={{ x: -200, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ delay: i * 0.2 }}
+                >
+                  <div className="border-b-2">{job.end}</div>
+                  <div>{job.start}</div>
+                </motion.td>
+                <motion.td
+                  className="w-32 border-l-2"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 9 }}
+                  transition={{ delay: i * 0.2 }}
+                >
+                  <Image
+                    loader={({ src, width }) => `${src}?w=${width}`}
+                    src={`work/${job.logo}.png`}
+                    alt=""
+                    width={100}
+                    height={100}
+                    className="my-2 ml-2 border"
+                  />
+                </motion.td>
+                <motion.td
+                  initial={{ x: 200, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ delay: i * 0.2 }}
+                >
+                  <h2 className="text-2xl">{job.company}</h2>
+                  <div className="text-black">{job.positon}</div>
+                  <div className="text-black">{job.location}</div>
+                </motion.td>
+              </tr>
+            ))}
+          </table>
+        </div>
       </div>
     </div>
   );
